f9cfe83358c55c2ef6ef75a1af808e2c2ce506fb
Jared Ross <jdross91@gmail.com>
2019-05-14 14:40:32 -0600

    Add an nlohmann-json as an embedded resource and link it against the exe

---
+++ MyExecutable/CMakeExample.cpp
@@ -0,0 +1,16 @@
+﻿// CMakeExample.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeExample.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	nlohmann::json t = { {"test","json"} };
+
+	cout << "Json: " << t;
+	return 0;
+}
